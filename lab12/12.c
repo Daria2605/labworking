230cee0e828dbfb7a122b297a6818d68e1d9d381
Daria2605 <144656784+Daria2605@users.noreply.github.com>
2023-12-11 01:38:01 +0300

    Update 12.c

--- lab12/12.c
+++ lab12/12.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main() {
+    int number, mn = 1, count = 1, output = 0;
+    printf("Введите число: ");
+    scanf("%d", &number);
+    while (number != 0)
+    {
+        if (count % 2 != 0)
+        {
+            output += number % 10 * mn;
+            mn *= 10;
+        }
+        number = number / 10;
+        
+        count += 1;
+    }
+    printf("Полученное число: %d\n", output);
+
+    return 0;
+}
